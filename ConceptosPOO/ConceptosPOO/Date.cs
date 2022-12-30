cbc3b4098c585d496fdff7d7ad46010f69a49364
Soporte <Soporte@SOPORTE-PC>
2022-12-30 10:32:37 -0300

    Clase Date finalizado.

--- ConceptosPOO/ConceptosPOO/Date.cs
+++ ConceptosPOO/ConceptosPOO/Date.cs
@@ -9,8 +9,40 @@
         public Date(int year, int month, int day)
         {
             _year = year;
-            _month = month;
-            _day = day;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeapyYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException("Día inválido");
+        }
+
+        private bool IsLeapyYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 == 0;
+            throw new NotImplementedException();
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException($"Mes inválido.");
         }
 
         public override string ToString()
--- ConceptosPOO/ConceptosPOO/Program.cs
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -2,4 +2,15 @@
 
 using ConceptosPOO;
 
-Console.WriteLine(new Date(2022, 7, 9));
+
+try
+{
+	Console.WriteLine(new Date(2022, 11, 9));
+    Console.WriteLine(new Date(1974, 9, 27));
+    Console.WriteLine(new Date(1985, 12, 45));
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
