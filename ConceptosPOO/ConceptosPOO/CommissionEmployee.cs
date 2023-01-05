31d1d973f209241496f25d45570b87b763037ed4
Soporte <Soporte@SOPORTE-PC>
2023-01-05 09:35:34 -0300

    Commission Employee

---
+++ ConceptosPOO/ConceptosPOO/CommissionEmployee.cs
@@ -0,0 +1,17 @@
+﻿namespace ConceptosPOO
+{
+    public class CommissionEmployee : Employee
+    {
+        // Creamos una variable para el porcentaje de comisión
+        public float CommissionPercentaje { get; set; }
+
+        // Creamos una variable para el monto total de ventas.
+        public decimal Sales { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            // Retornamos el valor de la comisión
+            return Sales * CommissionEmployee;
+        }
+    }
+}
